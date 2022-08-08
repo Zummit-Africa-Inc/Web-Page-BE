e69e3e92c46d75993ed1bd1e189cd858f6af1377
Chimexx <mezie.cjo@gmail.com>
2022-08-08 12:34:18 +0100

    Feature-Paystack webhook

--- src/common/Interfaces/mailDetails.interface.ts
+++ src/common/Interfaces/mailDetails.interface.ts
@@ -1,7 +1,7 @@
 export interface Details {
   email: string;
-  firstName?: string;
-  lastName?: string;
+  first_name?: string;
+  last_name?: string;
   amount: number;
   tutor: string;
   title: string;
