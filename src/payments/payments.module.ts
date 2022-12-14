6151fdbb34fc6662d45c8f6d26bb514c9a4f2688
Chimexx <mezie.cjo@gmail.com>
2022-12-14 22:43:40 +0100

    modified mail template

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -5,10 +5,16 @@ import { HttpModule } from '@nestjs/axios';
 import { CoursesRepository } from '../database/repository/courses.repository';
 import { MailModule } from 'src/mail/mail.module';
 import { PaymentDetailsRepository } from 'src/database/repository/payments.repository';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   imports: [HttpModule, MailModule],
   controllers: [PaymentsController],
-  providers: [PaymentService, CoursesRepository, PaymentDetailsRepository],
+  providers: [
+    PaymentService,
+    CoursesRepository,
+    PaymentDetailsRepository,
+    MailService,
+  ],
 })
 export class PaymentsModule {}
